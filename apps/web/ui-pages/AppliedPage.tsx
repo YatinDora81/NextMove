089089ce18b9f28b992588aff9a18a9c4b134812
YatinDora81 <yatin.dora81@gmail.com>
2026-01-02 00:46:33 +0530

    fix(web): update AppliedPage layout by removing fixed height and adding background color to the table for improved design

--- apps/web/ui-pages/AppliedPage.tsx
+++ apps/web/ui-pages/AppliedPage.tsx
@@ -16,9 +16,9 @@ function AppliedPage({ messages }: { messages: GeneratedMessage[] }) {
 
     return (
         <div className=' w-full min-h-screen pt-[8vh] md:pt-[12vh] flex justify-center items-center'>
-            <div className=' w-[80%] min-h-screen h-[200vh] flex flex-col justify-start items-start'>
+            <div className=' w-[80%] min-h-screen flex flex-col justify-start items-start'>
 
-                <Table>
+                <Table className=' bg-pink-500'>
                     <TableCaption>A list of your applied Companies.</TableCaption>
                     <TableHeader>
                         <TableRow>
